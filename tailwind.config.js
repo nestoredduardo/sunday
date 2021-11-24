e3f059e84314219e996a2ffde0076d4a362f834c
Nestor Mamani <nmamanipantoja@gmail.com>
2021-11-24 11:47:49 -0500

    🎨 Theme Colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,10 +3,22 @@ module.exports = {
   purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: '#7645D9',
+        secondary: '#58EFFF',
+        positive: '#E858FF',
+        background: '#EDF5FF',
+        gray: {
+          DEFAULT: '#9AA3A3',
+          hover: '#EFF4F5',
+          border: '#E2E8F0',
+        },
+      },
+    },
   },
   variants: {
     extend: {},
   },
   plugins: [],
-}
+};
