73f3771f907094d94104ac3af646a1c81aa53ff8
Nestor Mamani <nmamanipantoja@gmail.com>
2021-12-02 19:13:29 -0500

    Chart Labels

--- components/Projects/ProjectOverview.tsx
+++ components/Projects/ProjectOverview.tsx
@@ -1,4 +1,31 @@
+import { Doughnut } from 'react-chartjs-2'
+import { Chart, ArcElement } from 'chart.js'
+import ChartDataLabels from 'chartjs-plugin-datalabels'
+
 const ProjectOverview = () => {
+  Chart.register(ArcElement)
+  Chart.register(ChartDataLabels)
+
+  const data = {
+    options: {
+      plugins: {
+        datalabels: {
+          backgroundColor: '#FFFF',
+          'font.weight': 'bold',
+        },
+      },
+    },
+    labels: ['Hecho', 'En cola'],
+    datasets: [
+      {
+        label: 'Avance del proyecto',
+        data: [20, 10],
+        backgroundColor: ['#cb5eee', '#0cd7e4'],
+        hoverOffset: 4,
+      },
+    ],
+  }
+
   return (
     <section>
       <h2 className="font-bold text-lg">Objetivo:</h2>
@@ -7,6 +34,9 @@ const ProjectOverview = () => {
         segura y cómoda posible permitiéndoles a los usuarios ahorrar en sus
         gastos de suscripciones.
       </p>
+      <div className="w-2/3 mx-auto my-4">
+        <Doughnut data={data} />
+      </div>
     </section>
   )
 }
